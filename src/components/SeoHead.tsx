790f8a7d951fd9f68be2390e78a3c9704c4d62d5
zziri <jihzang@gmail.com>
2023-10-23 22:33:02 +0900

    homepage test

---
+++ src/components/SeoHead.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+interface Props {
+  title: string,
+  description: string
+}
+
+export default function SeoHead({ title, description }: Props) {
+  return <>
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  </>;
+}
+
