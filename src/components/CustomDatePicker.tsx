8246c84f3ed55f900d4a27df7f9430598d86b705
zziri <jihzang@gmail.com>
2023-10-25 02:07:55 +0900

    [hotfix/dday] bug fix

--- src/components/CustomDatePicker.tsx
+++ src/components/CustomDatePicker.tsx
@@ -11,7 +11,7 @@ interface CustomDatePickerProps {
 }
 
 export default function CustomDatePicker({ minDate, date, setDate }: CustomDatePickerProps) {
-  const years = range(getYear(date), getYear(date) + 50, 1);
+  const years = range(getYear(minDate), getYear(minDate) + 50, 1);
   const months = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'];
 
   return (
