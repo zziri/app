66eb3bf832c638743c904f0f5d800006f7dc562f
zziri <44119161+zziri@users.noreply.github.com>
2023-10-31 15:14:49 +0900

    [dev/random-pick] 랜덤 숫자 뽑기 (#18)

---
+++ src/components/extractor/random/RandomExtractorFunction.tsx
@@ -0,0 +1,93 @@
+import { isEmpty, range, shuffle, slice } from "lodash-es";
+import { ChangeEvent, useState } from "react";
+
+export default function RandomExtractorFunction() {
+  const [start, setStart] = useState<number>(1);
+  const [end, setEnd] = useState<number>(45);
+  const [count, setCount] = useState<number>(6);
+  const [list, setList] = useState<Array<number>>([]);
+
+  function onChangeStart(e: ChangeEvent<HTMLInputElement>) {
+    setStart(Number(e.target.value));
+  }
+
+  function onChangeEnd(e: ChangeEvent<HTMLInputElement>) {
+    setEnd(Number(e.target.value));
+  }
+
+  function onClickExtract(e: any) {
+    e.preventDefault();
+
+    const rangeList = range(start, end + 1);
+    const shuffled = shuffle(rangeList);
+    const sliced = slice(shuffled, 0, count);
+    setList(sliced);
+  }
+
+  function onChangeCount(e: any) {
+    setCount(e.target.value);
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="input range">
+          <span>숫자 범위: </span>
+          <input type="number" onChange={onChangeStart} value={start} />
+          <span> ~ </span>
+          <input type="number" onChange={onChangeEnd} value={end} />
+        </div>
+        <div className="input count">
+            <span>개수:</span>
+            <input type="number" value={count} onChange={onChangeCount}/>
+            <button onClick={onClickExtract}>뽑기</button>
+        </div>
+        <div className="result">
+          <span>
+            {!isEmpty(list) && list.map((value, index) => {
+              const suffix = (list.length - 1 === index) ? '' : ', ';
+              return `${value}${suffix}`;
+            })}
+          </span>
+        </div>
+      </div>
+      <style jsx>{`
+        .container * {
+          font-size: 1rem;
+        }
+        div {
+          {/* border: solid; */}
+        }
+        .input {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          padding: 1rem;
+          gap: 0.5rem;
+        }
+        .result {
+          padding: 1rem;
+        }
+        .result span {
+          font-size: 2rem;
+          font-weight: bold;
+        }
+        input {
+          max-width: 3.5rem;
+          height: 1rem;
+          border-radius: 0.5rem;
+          border-width: 0.1rem;
+          padding-left: 1rem;
+          padding-top: 0.5rem;
+          padding-bottom: 0.5rem;
+        }
+        button {
+          border-radius: 0.75rem;
+          padding: 0.5rem;
+          text-align: center;
+          font-weight: bold;
+        }
+      `}</style>
+    </>
+  );
+}
---
+++ src/pages/extractor/random.tsx
@@ -0,0 +1,26 @@
+import Description from "@/components/common/Description";
+import EmptyDiv from "@/components/common/EmptyDiv";
+import SeoArticle from "@/components/common/SeoArticle";
+import SeoHead from "@/components/common/SeoHead";
+import Title from "@/components/common/Title";
+import RandomExtractorFunction from "@/components/extractor/random/RandomExtractorFunction";
+import { randomArticleList } from "@/data/extractor/randomData";
+
+export default function RandomExtractorPage() {
+  const title = '랜덤 숫자 뽑기';
+  const description = `입력된 숫자 범위 내에서 임의의 정수를 무작위 추출합니다. 뽑은 숫자끼리는 중복되지 않습니다.
+  입력한 개수만큼 입력한 범위 내에서 랜덤으로 추출합니다. 여러 사람 중에 일부만 추첨해야 할 때 사용해 보세요.`;
+
+  return (
+    <>
+      <SeoHead
+        title={title}
+        description={description} />
+      <Title title={title}/>
+      <Description content={description} />
+      <EmptyDiv height={"2rem"} />
+      <RandomExtractorFunction />
+      <SeoArticle list={randomArticleList}/>
+    </>
+  );
+}
