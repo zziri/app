df323524a3789797264447205cd8a69c23863611
zziri <jihzang@gmail.com>
2023-11-06 02:05:41 +0900

    [dev/sitemap] sitemap 및 robots.txt 추가, 불필요한 페이지 삭제

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://app.zziri.me',
+  generateRobotsTxt: true,
+};
--- src/pages/counter/letter.tsx
+++
@@ -1,9 +0,0 @@
-export default function LetterCounter() {
-  return (
-    <>
-      <div>
-        글자 수 측정기
-      </div>
-    </>
-  );
-}
