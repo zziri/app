b40e36f390289a744d952e54749b4b8af496295d
zziri <jihzang@gmail.com>
2023-11-06 03:03:02 +0900

    [dev/markup-csat] 디데이 마크업 변경

--- src/components/d-day/DiffDayOutput.tsx
+++ src/components/d-day/DiffDayOutput.tsx
@@ -2,6 +2,17 @@ import { diffDayTargetDateState } from "@/atoms";
 import { differenceInDays, format, set } from "date-fns";
 import { useRecoilValue } from "recoil";
 
+/**
+   * 디데이 기준이므로 양일때 '-', 음일때 '+' 반대
+   */
+function getSign(value: number) {
+  return value >= 0 ? '-' : '+';
+}
+
+function getColor(value: number) {
+  return value >= 0 ? 'red' : 'blue';
+}
+
 export default function DiffDayOutput() {
   const diffDayTargetDate = useRecoilValue<Date>(diffDayTargetDateState);
 
@@ -17,20 +28,46 @@ export default function DiffDayOutput() {
 
   const dayDiff = differenceInDays(target, now);
 
-  /**
-   * 디데이 기준이므로 양일때 '-', 음일때 '+' 반대
-   */
-  function getSign(value: number) {
-    return value >= 0 ? '-' : '+';
-  }
-
-  function getColor(value: number) {
-    return value >= 0 ? 'red' : 'blue';
-  }
-
   return (
     <>
-    <ul className="list-group">
+      <div className="d-day-output-container">
+        <div className={`d-day-result bold ${getColor(dayDiff)}`}>
+          <span>D{getSign(dayDiff)}{Math.abs(dayDiff)}</span>
+        </div>
+        <div className="d-day-base">
+          <span>기준 날짜는 <strong>{format(now, 'yyyy년 MM월 dd일')}</strong> 입니다</span>
+        </div>
+        <div className="d-day-event">
+          <span>이벤트 날짜는 <strong>{format(diffDayTargetDate, 'yyyy년 MM월 dd일')}</strong> 입니다</span>
+        </div>
+      </div>
+
+      <style jsx>{`
+        .d-day-output-container > div {
+          {/* border: solid; */}
+          text-align: center;
+          padding: 0.5rem;
+        }
+        .d-day-result {
+          font-size: 3rem;
+        }
+        .d-day-base {
+          font-size: 1.2rem;
+        }
+        .d-day-event {
+          font-size: 1.2rem;
+        }
+        .bold {
+          font-weight: bold;
+        }
+        .red {
+          color: red;
+        }
+        .blue {
+          color: blue;
+        }
+      `}</style>
+      {/* <ul className="list-group">
         <li>
           <span>D-day</span>
           <span className={`bold ${getColor(dayDiff)}`}>D{getSign(dayDiff)}{Math.abs(dayDiff)}</span>
@@ -91,7 +128,7 @@ export default function DiffDayOutput() {
         .blue {
           color: blue;
         }
-      `}</style>
+      `}</style> */}
     </>
   );
 }
