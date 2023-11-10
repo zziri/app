a95d1a6e363b87e7f00ef817a1d31b1ac96de357
zziri <44119161+zziri@users.noreply.github.com>
2023-11-11 03:07:23 +0900

    [dev/ga] 구글 애널리틱스 적용 (#27)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MS6SVE9XJY"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-MS6SVE9XJY');
+          `}
+        </script>
+      </Head>
       <body>
         <Main />
         <NextScript />
