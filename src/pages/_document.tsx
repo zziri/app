79ce7b9757e8281c8412c80594f023b58eac3f1c
zziri <44119161+zziri@users.noreply.github.com>
2023-11-11 22:44:46 +0900

    [dev/ga-revert] 구글 애널리틱스 다시 제거 (#28)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,18 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MS6SVE9XJY"></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-MS6SVE9XJY');
-          `}
-        </script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
