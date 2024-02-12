360dd129ef00093e1ae6cbfa169f7066c4083772
zziri <jihzang@gmail.com>
2024-02-12 21:01:04 +0900

    [dev/dday-share] 알림 모달 컴포넌트

---
+++ src/pages/test/index.tsx
@@ -0,0 +1,25 @@
+// App.tsx 또는 다른 부모 컴포넌트
+import React, { useState } from 'react';
+import { Notice } from './Notice'; // Notice 컴포넌트 임포트
+
+const Test = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleModal = () => setOpen(!open);
+
+  return (
+    <div>
+      <button onClick={toggleModal}>Toggle Notice</button>
+      <Notice
+        open={open}
+        title="Important Notice"
+        content="This is an important message for you."
+        titleColor="darkblue" // 제목 색상
+        contentColor="darkgreen" // 내용 색상
+        onClose={toggleModal} // 모달 닫기 함수
+      />
+    </div>
+  );
+}
+
+export default Test;
