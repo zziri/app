d6a172669ed7299a01faa6d0977233d064693da1
zziri <44119161+zziri@users.noreply.github.com>
2024-11-01 19:38:19 +0900

    [hotfix/adsense1] 애드센스 핫픽스 (#54)

--- src/components/common/SeoHead.tsx
+++ src/components/common/SeoHead.tsx
@@ -12,6 +12,10 @@ export default function SeoHead({ title, description }: Props) {
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4184579550762583"
+        crossOrigin="anonymous"></script>
     </Head>
   </>;
 }
